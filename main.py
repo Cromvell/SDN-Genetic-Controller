17898cf96980b834d8cd726899f8d104009a3fdd
Cromvell <nverzn@gmail.com>
2019-05-03 21:41:36 +0300

    Add random graph generation and some minor things

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import genetic
 import random
 import datetime
@@ -8,13 +9,14 @@ def findAllPathsUtil(nodes, links, visited, node, goal, path, paths):
     visited[node] = True
     path.append(node)
 
-    if len(path) <= 3: # Condition for restriction path length (3 nodes: src -> med -> trg)
-        if goal == node:
-            paths.append(path[:])
-        else:
-            for child in getChildren(node, links):
-                if visited[child] == False:
-                    findAllPathsUtil(nodes, links, visited, child, goal, path, paths)
+    # !!! UNCOMENT FOR FULL-CONNECTED NETWORK !!!
+    #if len(path) <= 3: # Condition for restriction path length (3 nodes: src -> med -> trg)
+    if goal == node:
+        paths.append(path[:])
+    else:
+        for child in getChildren(node, links):
+            if visited[child] == False:
+                findAllPathsUtil(nodes, links, visited, child, goal, path, paths)
 
     path.pop()
     visited[node] = False
@@ -38,8 +40,7 @@ def getChildren(node, links):
     for l in links:
         if l[0] == node:
             children.append(l[1])
-        else:
-            if l[1] == node:
+        elif l[1] == node:
                 children.append(l[0])
     return children
 
@@ -81,6 +82,7 @@ def decodeGenotype(person):
     '''Decoding genotype into array of path indexes'''
     global numberStreams
     global streamsPaths
+    global numberBitsInGenotype
     personBinaryLenght = person.bit_length()
 
     '''
@@ -95,7 +97,7 @@ def decodeGenotype(person):
             if (i * numberBitsInGenotype // numberStreams + j) < personBinaryLenght:
                 extractNumber = extractNumber | ((person & extractConstant) >> i * numberBitsInGenotype // numberStreams)
                 extractConstant = extractConstant << 1
-        phenotype.append(extractNumber)
+        phenotype.append(int(extractNumber))
         extractNumber = 0
     phenotype.reverse()
 
@@ -121,8 +123,14 @@ def getNetworkLoad(phenotype):
 
     # Select paths corresponding to phenotype
     paths = []
-    for i in range(0, len(phenotype)):
-        paths.append(streamsPaths[i][phenotype[i]])
+    try:
+        for i in range(0, len(phenotype)):
+            if phenotype[i] >= 0:
+                paths.append(streamsPaths[i][phenotype[i]])
+    except Exception:
+        t, v, tb = sys.exc_info()
+        print("i: {}, phenotype[i]: {}".format(i, phenotype[i]))
+        raise t, v, tb
 
     linksTraffic = 0
     for i in range(0, len(links)):
@@ -141,13 +149,18 @@ def getNetworkLoad(phenotype):
 def SDNFitnessFun(person):
     '''Custom controller fitness function'''
 
-    # Decode genotype
-    phenotype = decodeGenotype(person)
+    try:
+        # Decode genotype
+        phenotype = decodeGenotype(person)
 
-    # Calculate network load
-    networkLoad = getNetworkLoad(phenotype)
+        # Calculate network load
+        networkLoad = getNetworkLoad(phenotype)
 
-    return float(penaltyFun(networkLoad))
+        return float(penaltyFun(networkLoad))
+    except Exception:
+        t, v, tb = sys.exc_info()
+        print(phenotype)
+        raise t, v, tb
 
 def inputParameter(msg, errorMsg, lowestValid):
     '''Helps input SDN parameters'''
@@ -166,6 +179,29 @@ def inputParameter(msg, errorMsg, lowestValid):
 
     return parameter
 
+def getRandomSpanningTree(nodes, links):
+    '''Gets random spanning tree for first network links generation'''
+
+    # Select root of tree randomly
+    checked = [random.randint(0, len(nodes)-1)]
+    tree = []
+    while len(checked) < len(nodes):
+
+        # Select first node in link only from cheked list
+        #   for sake of getting one tree instead of forest
+        i = random.randint(0, len(checked)-1)
+        # Next node can be whatever random wants but not a loop
+        j = random.randint(0, len(nodes)-1)
+        while checked[i] == nodes[j] or nodes[j] in checked:
+            j = random.randint(0, len(nodes)-1)
+
+        # Add link to tree if it not existed yet
+        if not (checked[i], nodes[j]) in tree and not (nodes[j], checked[i]) in tree:
+            tree.append((checked[i], nodes[j]))
+            checked.append(nodes[j])
+
+    return tree
+
 
 #
 # __main__
@@ -178,45 +214,20 @@ if __name__ == "__main__":
     nodesNumber = inputParameter("Enter number of nodes in SDN(>=3): ", "Please, enter valid number of nodes", 3)
     linksNumber = 0 # We assign value after generation full topology (linksNumber >= nodesNumber)
     streamsNumber = inputParameter("Enter number of streams in SDN(>=2): ", "Please, enter valid number of streams", 2) #TODO: Fix issue with one stream in the network
-    minLinkCapacity = inputParameter("Enter minimum link capacity(must be not less than 10): ", "Please, enter valid number of minimum link capacity", 10)
-    maxLinkCapacity = inputParameter("Enter maximum link capacity(>(minLinkCap + 20)): ", "Please, enter valid number of maximum link capacity", minLinkCapacity + 20)
-
+    minLinkCapacity = 10#inputParameter("Enter minimum link capacity(must be not less than 10): ", "Please, enter valid number of minimum link capacity", 10)
+    maxLinkCapacity = 100#inputParameter("Enter maximum link capacity(>(minLinkCap + 20)): ", "Please, enter valid number of maximum link capacity", minLinkCapacity + 20)
 
     # Generate nodes
     nodes = []
     for i in range(0, nodesNumber):
         nodes.append(i)
 
-    '''
-    # Generate random-connected network links
-    links = []
-    for i in range(0, linksNumber):
-        while True:
-            # At first we connected all nodes together, further we use random generation
-            if i < nodesNumber:
-                nodeFirst = nodes[i]
-            else:
-                nodeFirst = random.randint(0, nodesNumber - 1)
-            if i < nodesNumber - 1:
-                nodeSecond = nodes[i + 1]
-            else:
-                nodeSecond = random.randint(0, nodesNumber - 1)
-                while nodeFirst == nodeSecond:
-                    nodeSecond = random.randint(0, nodesNumber - 1)
-            if not ((nodeFirst, nodeSecond) in links or (nodeSecond, nodeFirst) in links):
-                break
-        links.append(tuple([nodeFirst, nodeSecond]))
-    '''
-
     # Generate full-connected network links
-    links = []
+    linksFull = []
     for i in range(0, nodesNumber):
         for j in range(0, nodesNumber):
-            if i != j and not ((i, j) in links or (j, i) in links):
-                links.append(tuple([i, j]))
-
-    # And at now we can gen number of links
-    linksNumber = len(links)
+            if i != j and not ((i, j) in linksFull or (j, i) in linksFull):
+                linksFull.append(tuple([i, j]))
 
     sumTotalExecutionTimes = 0
     sumGAExecutionTimes = 0
@@ -224,12 +235,31 @@ if __name__ == "__main__":
     firstBest = 0
     inadequateCount = 0
     diffMean = 0
+    redoCountMean = 0
     print("--------------------------------------------------------------------")
     #
     # Algorithm test loop
     #
     for iteration in range(0, 100):
         print("ITERATION #{0}".format(iteration))
+
+        # Generate links at first as tree
+        links = getRandomSpanningTree(nodes, linksFull)
+
+        # And then generate additional bounds
+        linksNodesRatio = 1.3
+        nodeNum = len(nodes)
+        while len(links) < nodeNum * linksNodesRatio: 
+            i = random.randint(0, nodeNum-1)
+            j = random.randint(0, nodeNum-1)
+            while i == j:
+                j = random.randint(0, nodeNum-1)
+
+            if not (i, j) in links and not (j, i) in links:
+                links.append((i, j))
+
+        # And at now we can gen number of links
+        linksNumber = len(links)
 
         # Generate link capacities
         linksCapacities = []
@@ -261,6 +291,8 @@ if __name__ == "__main__":
             baz = findAllPaths(nodes, links, s[0], s[1])[:]
             streamsPaths.append(baz)
 
+        print("STREAM PATHS: {}".format(streamsPaths))
+
         # Calculate array of path variants numbers for util purposes
         numbersPaths = [];
         for s in streamsPaths:
@@ -269,14 +301,31 @@ if __name__ == "__main__":
         # Calculate number bits in genotype
         numberStreams = len(streams)
         numberBitsInGenotype = getBitsNum(max(numbersPaths), numberStreams)
+        print("NUMBER BITS IN GENOTYPE: {}".format(numberBitsInGenotype))
 
         # Fix time of preparing algorithms stop and fix time of the GA start
         prepareStopTime = datetime.datetime.now()
         GAStartTime = datetime.datetime.now()
 
-        # Execution of the genetic algorithm and receive answer from it
-        answer = genetic.executeGeneticAlgoritm(SDNFitnessFun, numberBitsInGenotype, 100, 0.003, True, firstBest)
-        phenotype = decodeGenotype(answer)
+        redoCount = 0
+        while True:
+            # Execution of the genetic algorithm and receive answer from it
+            answer = genetic.executeGeneticAlgoritm(SDNFitnessFun, numberBitsInGenotype, 100, 0.003, True, firstBest)
+            phenotype = decodeGenotype(answer)
+
+            # Calculate network loads before and after optimisation
+            randomSolutionLoad = getNetworkLoad(decodeGenotype(firstBest)) * 100
+            bestSolutionLoad = getNetworkLoad(decodeGenotype(answer)) * 100
+
+            # If result is adequate - proceed, else redo genetic algorithm
+            if randomSolutionLoad >= bestSolutionLoad or redoCount > 10:
+                break
+
+            redoCount += 1
+        
+        print("CHROMOSOME: {:b}".format(answer))
+
+        redoCountMean += redoCount
 
         # Fix time of the GA stop and calculate execution times
         GAStopTime = datetime.datetime.now()
@@ -297,26 +346,14 @@ if __name__ == "__main__":
         for i in range(0, len(randomPhenotype)):
             randomPaths.append(streamsPaths[i][randomPhenotype[i]])
 
-        '''
-        # Get fitnesses before and arter optimisation
-        randomSolutionFitness = SDNFitnessFun(firstBest)
-        bestSolutionFitness = SDNFitnessFun(answer)
-
-        if randomSolutionFitness < bestSolutionFitness:
-            inadequateCount += 1
-        '''
-
-        # Calculate network loads before and after optimisation
-        randomSolutionLoad = getNetworkLoad(decodeGenotype(firstBest)) * 100
-        bestSolutionLoad = getNetworkLoad(decodeGenotype(answer)) * 100
-
         if randomSolutionLoad < bestSolutionLoad:
             inadequateCount += 1
             print("\n!!!INADEQUATE RESULT DETECTED!!!")
         else:
             difference = randomSolutionLoad - bestSolutionLoad
-            print("Genetic and random result difference: {0:.3}%".format(difference))
+            print("\nGenetic and random result difference: {0:.3}%".format(difference))
             diffMean += difference
+        print("Redo count: {0}\n".format(redoCount))
 
         print("\nRandom selection of paths: {0}".format(randomPaths))
         print("Best selection of paths: {0}".format(paths))
@@ -328,9 +365,11 @@ if __name__ == "__main__":
         print("--------------------------------------------------------------------")
 
     diffMean /= 100
+    redoCountMean /= 100
     print("\nSUMMARY:")
     print("Average execution time of prepare algorithms equal {0:.6} milliseconds".format(sumPrepareExecutionTimes * 1000 / 100))
     print("Average execution time of the genetic algorithm equal {0:.6} milliseconds".format(sumGAExecutionTimes * 1000 / 100))
     print("Average total execution time equal {0:.6} milliseconds".format(sumTotalExecutionTimes * 1000 / 100))
     print("Number of inadequate results: {0}".format(inadequateCount))
     print("Genetic and random mean result difference: {0:.3}%".format(diffMean))
+    print("Mean redo of inadequate result: {0}".format(redoCountMean))
